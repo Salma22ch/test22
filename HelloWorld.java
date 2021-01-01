2fe13e2da1b3320bfb9814e10d37a3aeefa8b661
Salma22ch <57417762+Salma22ch@users.noreply.github.com>
2021-01-01 00:25:14 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
-    public static void HelloWorld(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World! TP3 BY CHANA -- LKHADIR"); 
     }
 }
